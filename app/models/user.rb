ff5e04c9930bba9dec5fa99ccd9ebbc9e4567433
matsuno1818 <matsuno.812@gmail.com>
2020-09-13 23:32:32 +0900

    userモデルのバリデーション変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
-  has_one :order
        
   Nickname = /\A[ぁ-んァ-ン一-龥a-z0-9]+\z/i 
   Email = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   Password = /\A[a-z0-9]+\z{6,}/i
   Zenkaku = /\A[ぁ-んァ-ン一-龥]/
-  Zenkakukana = /\A[ァ-ヶー－]+\z/
          
        
   with_options presence: true do
@@ -19,7 +17,6 @@ class User < ApplicationRecord
     validates :email, uniqueness: true, format: { with: Email, message: "Email has already been taken" }
     validates :password, format: { with: Password, message: "Password Include both letters and numbers" }
     validates :last_name, :first_name, format: { with: Zenkaku, message: "Full-width characters" }
-    validates :last_name_reading, :first_name_reading, format: { with: Zenkakukana, message: " Full-width katakana characters" }
     validates :birthday
   end
 end
